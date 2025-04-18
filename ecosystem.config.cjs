192ac616984cc3eb87a001e4a0cd50c2da22f68b
Vassil Shterev <vsterev@gmail.com>
2025-04-18 20:06:04 +0300

    add pm2 config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: "shbm-be",
+      script: "./dist/src/app.js",
+      instances: 1,
+      wait_ready: true,
+      exec_mode: "cluster",
+    },
+  ],
+  deploy: {
+    production: {
+      user: "vsterev",
+      host: "192.168.10.10",
+      ref: "origin/master",
+      repo: "git@github.com:vsterev/shbm-be.git",
+      path: "/home/vsterev/git/shbm/pm2/shbm-backend",
+      "post-deploy": "yarn && yarn build && pm2 startOrReload ecosystem.config.cjs --only shbm_hotel_proxy",
+    },
+  },
+};
